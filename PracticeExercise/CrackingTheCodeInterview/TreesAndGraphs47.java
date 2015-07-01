3106d46569c751a46ce6ca99fbfea711d84f1317
Nguyen Tuan Anh <hamsterviel01@gmail.com>
2015-07-01 09:31:43 +0800

    End of 30/06

--- PracticeExercise/CrackingTheCodeInterview/TreesAndGraphs47.java
+++ PracticeExercise/CrackingTheCodeInterview/TreesAndGraphs47.java
@@ -1,6 +1,34 @@
 public class TreesAndGraphs47 {
 	public BinaryTreeNode searchFirstValue(BinaryTreeNode node, int val1, int val2){
-		
+		while (node != null){
+			if (node.getValue() == val1 || node.getValue() == val2){
+				return node;
+			} else {
+				searchFirstValue(node.getLeftChild(), val1, val2);
+				searchFirstValue(node.getRightChild(), val1, val2);
+			}
+		}
+		return null;
+	}
+
+	public BinaryTreeNode findAllChild(BinaryTreeNode node, int val){
+
+	}
+
+	public BinaryTreeNode findAllInAncestors(BinaryTreeNode node, int val){
+
+	}
+
+	public BinaryTreeNode findCommonAncestor(BinaryTreeNode root, int val1, int val2){
+		BinaryTreeNode firstMatch = searchFirstValue(root, val1, val2);
+		if (firstMatch == null) return null;
+		if (val1 == val2) return (BinaryTreeNode)firstMatch;.getParent();
+
+		int matchedVal = (firstMatch.getValue() == val1)?val1:val2;
+		int findingVal = (firstMatch.getValue() == val1)?val2:val1;
+
+
+
 	}
 
 	public BinaryTreeNode searchFromNode(BinaryTreeNode node, BinaryTreeNode commonAncestor, int val){
