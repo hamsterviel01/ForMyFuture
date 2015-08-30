4c555cb4a383959696c0a27a22bd6bfdf7b4beb3
Nguyen Tuan Anh <hamsterviel01@gmail.com>
2015-08-30 16:35:29 +0800

    End of 30/08

--- Online Exam/Works Applications/Exam1/Main.java
+++ Online Exam/Works Applications/Exam1/Main.java
@@ -2,126 +2,168 @@ import java.util.*;
 import java.io.*;
 
 /**
-There are few problems right now:
-- About point -1: cannot return value as -1 because then what happen if all direction is blocked , it will return max value as current point -1, and if current point is large enough, it can still be maximum value without ever reaching the other end
-- If I allow the snake to go up and down, it will create a loop, many terrible loop that will cause this program to never stop --> to to resolve this issue?
-
-Using path to check for loop??
-if next point is already in the path, exluded it.
-
+Optimal structure:
+maxPoint(x, y) = matrix[x][y] + maxValue(maxPoint(x, y+1), maxPoint(x-1, y, exclude(listOfVisistedPoint)), maxPoint(x+1, y, exclude(listOfVisistedPoint)));
 */
 
 public class Main {
+	public static long findMaximumPoint(int[][] matrixPoint){
+		int maxX = matrixPoint.length-1;
+		long[][] cache = new long[matrixPoint.length][matrixPoint[0].length];
+		long[][] cacheUp = new long[matrixPoint.length][matrixPoint[0].length];
+		long[][] cacheDown = new long[matrixPoint.length][matrixPoint[0].length];
+		for (int i=0; i<cache.length; i++){
+			for (int j=0; j<cache[0].length; j++){
+				cache[i][j] = -2;
+				cacheUp[i][j] = -2;
+				cacheDown[i][j] = -2;
+			}	
+		}
+
+		long max = -1;
+		for (int i=0; i<matrixPoint.length; i++){
+			long temp = findMaximumPoint(matrixPoint, i, 0, cache, cacheUp, cacheDown);
+			if (temp > max){
+				max = temp;
+			}
+		}
+
+		//Usual algorithm will never reach these point, but the snake can still access it through teleport, so we still need to consider it
+		//Dont need to care if value of this point is -1, it will be checked at findMaximumPoint func
+		//Value of matrixPoint at y = 0 has been taken care of, now we only need to care about y = 1 and onward
+		for (int j=1; j<matrixPoint[0].length; j++){
+			if (cacheUp[0][j] == -2 && cache[0][j] == -2 && (cacheDown[maxX][j] != -2 || cache[maxX][j] != -2) && matrixPoint[maxX][j] != -1){
+				long temp = findMaximumPoint(matrixPoint, 0, j, cache, cacheUp, cacheDown);
+				if (temp > max){
+					max = temp;
+				}
+			}
+			if (cacheDown[maxX][j] == -2 && cache[maxX][j] == -2 && (cacheUp[0][j] != -2 || cache[0][j] != -2) && matrixPoint[0][j] != -1){
+				long temp = findMaximumPoint(matrixPoint, maxX, j, cache, cacheUp, cacheDown);
+				if (temp > max){
+					max = temp;
+				}
+			}		
+		}
+
+		return max;
+	}
+
 	//Need to check if number of input match m and n
-	public static int findMaximumPoint(int[][] matrixPoint, int x, int y, int[][] cache, ArrayList<Point> path){
-		//if the snake has gone outside the matrix
+	public static long findMaximumPoint(int[][] matrixPoint, int x, int y, long[][] cache, long[][] cacheUp, long[][] cacheDown){
+		//if the snake has gone outside the matrix, if return -1 here, the whole cache will become -1.
 		if (y > matrixPoint[0].length-1){
 			return 0;
 		}
 		//if this position has been calculated
 		if (cache[x][y] != -2) return cache[x][y];
-
 		//if this position is blocked
 		if (matrixPoint[x][y] == -1){
+			cacheUp[x][y] = -1;
 			cache[x][y] = -1;
+			cacheDown[x][y] = -1;
 			return cache[x][y];
-		}
+		}	
 
-		//if the snake is at top or bottom
-		System.out.println(path);
-		if (x == 0) {
-			int currentPoint = matrixPoint[x][y];
-			for (Point p: path){
-				currentPoint =  currentPoint + matrixPoint[p.x][p.y];
-			}
-			path.add(new Point(x, y));
-			int temp = findMaximumPoint(matrixPoint, matrixPoint.length-1, y, cache, path);
-			return max(currentPoint, temp);
-		} else if (x == matrixPoint.length-1){
-			int currentPoint = matrixPoint[x][y];
-			for (Point p: path){
-				currentPoint =  currentPoint + matrixPoint[p.x][p.y];
-			}
-			path.add(new Point(x, y));
-			int temp = findMaximumPoint(matrixPoint, 0, y, cache, path);
-			return max(currentPoint, temp);
-		}
-
-		//if the snake has reached right border
-		if (y == matrixPoint[0].length){
-			path.add(new Point(x, y));
-			cache[x][y] = matrixPoint[x][y] + max(findMaximumPoint(matrixPoint, x-1, y, cache, path), 
-				findMaximumPoint(matrixPoint, x+1, y, cache, path));
-			return cache[x][y];
-		}
-
-		path.add(new Point(x, y));
-		int max = max(findMaximumPoint(matrixPoint, x-1, y, cache, path), findMaximumPoint(matrixPoint, x+1, y, cache, path), 
-			findMaximumPoint(matrixPoint, x, y+1, cache, path));
+		long up = findMaximumPointUp(matrixPoint, x-1, y, cache, cacheUp, cacheDown);
+		long down = findMaximumPointDown(matrixPoint, x+1, y, cache, cacheUp, cacheDown);
+		long right = findMaximumPoint(matrixPoint, x, y+1, cache, cacheUp, cacheDown);
+		long tempMax = Math.max(up, down);
+		long max = Math.max(tempMax, right);
 		if (max == -1){
-			cache[x][y] = -1;
-			return cache[x][y];
+			cache[x][y] = -1;		
 		} else {
 			cache[x][y] = max + matrixPoint[x][y];
-			return cache[x][y];
 		}
+		return cache[x][y];
 	}
 
-	public static void checkForLoop(int x, int y, ArrayList<>)
-
-	public static int max(int... values){
-		//equal to -1 because the smallest value in matrixPoint is -1
-		int max = -1;
-		for (int i: values){
-			if (i > max){
-				max = i;
-			}
+	public static long findMaximumPointUp(int[][] matrixPoint, int x, int y, long[][] cache, long[][] cacheUp, long[][] cacheDown){
+		//if the snake has gone outside the matrix
+		if (y > matrixPoint[0].length-1){
+			return 0;
 		}
-		return max;
+		if (x < 0){
+			return -1;
+		}
+		//if this position has been calculated
+		if (cacheUp[x][y] != -2) return cacheUp[x][y];
+		//if this position is blocked
+		if (matrixPoint[x][y] == -1){
+			cacheUp[x][y] = -1;
+			cache[x][y] = -1;
+			cacheDown[x][y] = -1;
+			return cache[x][y];
+		}
+
+		long right = findMaximumPoint(matrixPoint, x, y+1, cache, cacheUp, cacheDown);
+		long up = findMaximumPointUp(matrixPoint, x-1, y, cache, cacheUp, cacheDown);
+		long max = Math.max(right, up);
+		if (max == -1){
+			cacheUp[x][y] = -1;
+		} else {
+			cacheUp[x][y] = matrixPoint[x][y] + max;
+		}
+		return cacheUp[x][y];
+	}
+
+	public static long findMaximumPointDown(int[][] matrixPoint, int x, int y, long[][] cache, long[][] cacheUp, long[][] cacheDown){
+		//if the snake has gone outside the matrix
+		if (y > matrixPoint[0].length-1){
+			return 0;
+		}
+		int maxX = matrixPoint.length-1;
+		if (x > maxX){
+			return -1;
+		}
+		//if this position has been calculated
+		if (cacheDown[x][y] != -2) return cacheDown[x][y];
+		if (matrixPoint[x][y] == -1){
+			cacheUp[x][y] = -1;
+			cache[x][y] = -1;
+			cacheDown[x][y] = -1;
+			return cache[x][y];
+		}
+		
+		long right = findMaximumPoint(matrixPoint, x, y+1, cache, cacheUp, cacheDown);
+		long down = findMaximumPointDown(matrixPoint, x+1, y, cache, cacheUp, cacheDown);
+		long max = Math.max(right, down);
+		if (max == -1){
+			cacheDown[x][y] = -1;
+		} else {
+			cacheDown[x][y] = matrixPoint[x][y] + max;
+		}
+		cacheDown[x][y] = matrixPoint[x][y] + Math.max(right, down);
+		return cacheDown[x][y];
 	}
 
 	public static void main(String[] args) {
 		// TODO: Implement your program
+		BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+		
 		try {
-			BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
-
-			//Do I need to check if only 2 int was input in the first line???
 			String[] line = rd.readLine().split(" ");
+			int numberOfInputLine = Integer.valueOf(line[0]);
 			int[][] matrix = new int[Integer.valueOf(line[0])][Integer.valueOf(line[1])];
-			int[][] cache = new int[Integer.valueOf(line[0])][Integer.valueOf(line[1])];
-
-			for (int i=0; i<cache.length; i++){
-				for (int j=0; j<cache[0].length; j++){
-					cache[i][j] = -2;
-				}
-			}
 			int i = 0;
 			String inputLine;
-			while ((inputLine = rd.readLine()) != null && inputLine.length() > 0){
+
+			while (i < numberOfInputLine && (inputLine = rd.readLine()) != null && inputLine.length() > 0){
 				String[] input = inputLine.split(" ");
 				for(int j=0; j<input.length; j++){
 					matrix[i][j] = Integer.valueOf(input[j]);
 				}
 				i++;
 			}
-
-			/**
-
-			FAIL WHEN MATRIX[0][0] = -1
-
-			*/
-			System.out.println(findMaximumPoint(matrix, 0, 0, cache, new ArrayList<Point>()));
+			System.out.println(findMaximumPoint(matrix));
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				rd.close();
+			} catch (IOException e){
+				e.printStackTrace();
+			}	
 		}
-	}
-}
-
-class Point {
-	int x;
-	int y;
-	public Point(int x, int y){
-		this.x = x;
-		this.y = y;
 	}
 }
---
+++ Online Exam/Works Applications/Exam2/Main.java
@@ -0,0 +1,95 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	//First, since graph has n vertices and n-1 edges and all cities connect to each other --> it is a connected and simple graph.
+	public static int calculateShortestPath(int visitorCity, HashMap<Integer, ArrayList<Integer>> islandMap, boolean[] isFestive){
+		boolean[] visited = new boolean[isFestive.length];
+		City currentCity = new City(visitorCity, 0);
+		LinkedList<City> waitList = new LinkedList<City>();
+
+		while (!isFestive[currentCity.name]){
+			//set visited to true;
+			visited[currentCity.name] = true;
+			for (int i: islandMap.get(currentCity.name)){
+				//if city is not visited yet
+				if (!visited[i]){
+					waitList.add(new City(i, currentCity.distance + 1));
+				}
+			}
+			currentCity = waitList.remove();
+		}
+		return currentCity.distance;
+	}
+
+	public static void main(String[] args) {
+		// TODO: Implement your program 
+		HashMap<Integer, ArrayList<Integer>> islandMap = new HashMap<Integer, ArrayList<Integer>>();
+		ArrayList<Integer> output = new ArrayList<Integer>();
+		BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+
+		try {		
+			String line[] = rd.readLine().split(" ");
+			int n = Integer.valueOf(line[0]);
+			int m = Integer.valueOf(line[1]);
+			boolean[] isFestive = new boolean[n+1];		
+			int count = 1;
+
+			//At first only city 1 is festive
+			isFestive[1] = true;
+			while (count <= n-1){
+				String[] temp = rd.readLine().split(" ");
+				int a = Integer.valueOf(temp[0]);
+				int b = Integer.valueOf(temp[1]);
+				if (!islandMap.containsKey(a)){
+					islandMap.put(a, new ArrayList<Integer>(Arrays.asList(b)));
+				} else {
+					islandMap.get(a).add(b);
+				}
+				
+				if (!islandMap.containsKey(b)){
+					islandMap.put(b, new ArrayList<Integer>(Arrays.asList(a)));
+				} else {
+					islandMap.get(b).add(a);
+				}
+				count++;
+			}
+
+			String inputLine;
+			while (count <= n+m-1 && (inputLine = rd.readLine()) != null && inputLine.length() > 0){
+				String[] temp = inputLine.split(" ");
+				int q = Integer.valueOf(temp[0]);
+				int c = Integer.valueOf(temp[1]);
+				if (q == 1){
+					isFestive[c] = true;
+				} else if (q == 2) {
+					int result = calculateShortestPath(c, islandMap, isFestive);
+					output.add(result);
+				}
+				count++;
+			}
+
+			//Print output
+			for (int i: output){
+				System.out.println(i);
+			}
+		} catch (IOException e){
+			e.printStackTrace();
+		} finally {
+			try {
+				rd.close();
+			} catch (IOException e){
+				e.printStackTrace();
+			}	
+		}
+	}
+}
+
+class City {
+	int name;
+	int distance;
+	City(int name, int distance){
+		this.name = name;
+		this.distance = distance;
+	}
+}
